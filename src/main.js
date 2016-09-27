7e08055c63516f9aba440ccbbffec902911cb236
bhushanjade7 <bhushanjade712@gmail.com>
2016-09-27 13:12:22 +0530

    jQuery ref

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
  * Created by bhushan on 26/9/16.
  */
 
+$ = jQuery = require('jquery');  // ref jquery as $ or jquery. global for bootstrap
 var App = console.log("Hellow world from Browsify");
 
 
